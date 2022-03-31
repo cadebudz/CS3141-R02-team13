5c8ae974130ee42fd701e360bc10e892d1ee28db
Tbetters <tlbetter@mtu.edu>
2022-03-31 18:48:52 -0400

    Converted all methods in main to take input and return strings instead of taking and sending to the terminal. This is so they can be implemented to the back end of the GUI

--- Nutrition Tracker/src/Main.java
+++ Nutrition Tracker/src/Main.java
@@ -13,104 +13,83 @@ public class Main {
 					2655, 2709, 2764, 2818, 2873, 2927 } };
 	final int[][] pro = {{110,121,132,143,154,165,176,187,198,209,220,231,243},
 			             {100,110,120,130,140,150,160,170,180,190,200,210,220}};
-	static Scanner scan = new Scanner(System.in);
 
+	//Functions updated to return strings to be used in GUI
 	public static void main(String[] args) {
-		getCalories();
+		/*getCalories();
 		getSodium();
 		getCarbohydrates();
 		getProtein();
-		getSugar();
-		scan.close();
+		getSugar();*/
 	}
 
-	public static void getCalories() {
+	public static String getCalories(int ical) {
 		final int cal = 2500;
 		int dif = 0;
-		System.out.println("How many calories did you eat today?");
-		while (!scan.hasNextInt())
-			scan.next();
-		int ical = scan.nextInt();
 		if (ical > cal) {
 			dif = ical - cal;
-			System.out.printf("You need to eat %d less calories.\n", dif);
+			return ("You need to eat "+dif+" less calories.\n");
 		} else if (ical < cal) {
 			dif = cal - ical;
-			System.out.printf("You need to eat %d more calories.\n", dif);
+			return ("You need to eat "+dif+" more calories.\n");
 		} else {
-			System.out.println("You are eating the correct amount of calories.");
+			return ("You are eating the correct amount of calories.");
 		}
 	}
 
-	public static void getSodium() {
+	public static String getSodium(int isod) {
 		final int sod = 2000;
 		int dif = 0;
-		System.out.println("How much sodium did you eat today? (mg)");
-		while (!scan.hasNextInt())
-			scan.next();
-		int isod = scan.nextInt();
 		if (isod > sod) {
 			dif = isod - sod;
-			System.out.printf("You need to eat %d less mg of sodium.\n", dif);
+			return ("You need to eat "+dif+" less mg of sodium.\n");
 		} else if (isod < sod) {
 			dif = sod - isod;
-			System.out.printf("You need to eat %d more mg of sodium.\n", dif);
+			return ("You need to eat "+dif+" more mg of sodium.\n");
 		} else {
-			System.out.println("You are eating the correct amount of sodium.");
+			return ("You are eating the correct amount of sodium.");
 		}
 	}
 
-	public static void getCarbohydrates() {
+	public static String getCarbohydrates(int icar) {
 		final int car = 250;
 		int dif = 0;
-		System.out.println("How much carbohydrates did you eat today? (g)");
-		while (!scan.hasNextInt())
-			scan.next();
-		int icar = scan.nextInt();
 		if (icar > car) {
 			dif = icar - car;
-			System.out.printf("You need to eat %d less g of carbohydrates.\n", dif);
+			return ("You need to eat "+dif+" less g of carbohydrates.\n");
 		} else if (icar < car) {
 			dif = car - icar;
-			System.out.printf("You need to eat %d more g of carbohydrates.\n", dif);
+			return ("You need to eat "+dif+" more g of carbohydrates.\n");
 		} else {
-			System.out.println("You are eating the correct amount of carbohydrates.");
+			return ("You are eating the correct amount of carbohydrates.");
 		}
 	}
 
-	public static void getProtein() {
+	public static String getProtein(int ipro) {
 		final int pro = 50;
 		int dif = 0;
-		System.out.println("How much protein did you eat today? (g)");
-		while (!scan.hasNextInt())
-			scan.next();
-		int ipro = scan.nextInt();
 		if (ipro > pro) {
 			dif = ipro - pro;
-			System.out.printf("You need to eat %d less g of protein.\n", dif);
+			return ("You need to eat "+dif+" less g of protein.\n");
 		} else if (ipro < pro) {
 			dif = pro - ipro;
-			System.out.printf("You need to eat %d more g of protein.\n", dif);
+			return ("You need to eat "+dif+" more g of protein.\n");
 		} else {
-			System.out.println("You are eating the correct amount of protein.");
+			return ("You are eating the correct amount of protein.");
 		}
 	}
 
-	public static void getSugar() {
+	public static String getSugar(int isug) {
 		final int sug = 36;
 		int dif = 0;
-		System.out.println("How much sugar did you eat today? (g)");
-		while (!scan.hasNextInt())
-			scan.next();
-		int isug = scan.nextInt();
 		if (isug > sug) {
 			dif = isug - sug;
-			System.out.printf("You need to eat %d less g of sugar.\n", dif);
+			return ("You need to eat "+dif+" less g of sugar.\n");
 		} else if (isug < sug) {
 			dif = sug - isug;
-			System.out.printf("You need to eat %d more g of sugar.\n", dif);
+			return ("You need to eat "+dif+" more g of sugar.\n");
 		} else {
-			System.out.println("You are eating the correct amount of sugar.");
+			return ("You are eating the correct amount of sugar.");
 		}
 	}
 	// returns negative if less calories are needed, positive if more calories are needed
